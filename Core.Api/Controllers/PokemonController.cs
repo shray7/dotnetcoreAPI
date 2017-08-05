049db532c57db36fd31bc6470e8eedd27f11b7a8
Shray <shray7@gmail.com>
2017-08-04 22:57:17 -0400

    Initial Commit

---
+++ Core.Api/Controllers/PokemonController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using Core.Api.Filters;
+
+namespace Core.Api.Controllers
+{
+    [LogTimeFilter]
+    [Route("api/[controller]")]
+    public class PokemonController : Controller
+    {
+        private JArray PokemonList;
+        public PokemonController()
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string jsonLocation = Path.Combine(currentDirectory, "pokemon.json");
+            PokemonList = JArray.Parse(System.IO.File.ReadAllText(jsonLocation));
+        }
+
+        // GET api/values
+        [HttpGet]
+        public JsonResult Get()
+        {
+            return Json(PokemonList);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")] 
+        public JsonResult Get(int id)
+        {
+            if (id < 1 || id > 722) return Json("{Invalid Request}");
+            return Json(PokemonList[id-1]);
+        }
+
+        // POST api/values
+        //[HttpPost]
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        // PUT api/values/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        // DELETE api/values/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ Core.Api/Filters/LogTimeFilter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.Api.Filters
+{
+    public class LogTimeFilter : Attribute, IActionFilter
+    {
+        public LogTimeFilter()
+        {
+
+        }
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            Debug.WriteLine(string.Format("Action Method {0} executed at {1}", context.ActionDescriptor.DisplayName, DateTime.Now.ToString()), "Web API Logs");
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            Debug.WriteLine(string.Format("Action Method {0} executing at {1}", context.ActionDescriptor.DisplayName, DateTime.Now.ToString()), "Web API Logs");
+        }
+    }
+}
