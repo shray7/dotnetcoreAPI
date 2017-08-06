a60e588a3a9fd3caa749fcc701b4f58b0d8e04d4
Shray <Shray@DESKTOP-AS127BC>
2017-08-06 16:11:00 -0400

    Added airbnb listing model

---
+++ Core.DataLayer/CosmosContext.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core.DataLayer
+{
+    public class CosmosContext : IDisposable
+    {
+
+        private bool disposed = false;
+
+        private string connectionString = "mongodb://shraycore:VNt0xl5HNH5xXzXk38VsSlSeRarBMEkKl8o4itgwMY8NSmI9uFYCxdhDQ6wtcsFJN1Z63prYIhpKiD6gtgEwBw==@shraycore.documents.azure.com:10255/?ssl=true&replicaSet=globaldb";
+        private string userName = "shraycore";
+        private string host = "shraycore.documents.azure.com";
+        private string password = "VNt0xl5HNH5xXzXk38VsSlSeRarBMEkKl8o4itgwMY8NSmI9uFYCxdhDQ6wtcsFJN1Z63prYIhpKiD6gtgEwBw==";
+
+        // This sample uses a database named "Tasks" and a 
+        //collection named "TasksList".  The database and collection 
+        //will be automatically created if they don't already exist.
+        private string dbName = "CoreDocument";
+        private string collectionName = "AirbnbListing";
+
+        // Gets all Task items from the MongoDB server.        
+        //public List<AirbnbListing> GetAllTasks()
+        //{
+            //    try
+            //    {
+            //        var collection = GetTasksCollection();
+            //        return collection.Find(new BsonDocument()).ToList();
+            //    }
+            //    catch (MongoConnectionException)
+            //    {
+            //        return new List<MyTask>();
+            //    }
+            //return null;
+        //}
+
+        // Creates a Task and inserts it into the collection in MongoDB.
+        //public void CreateTask(MyTask task)
+        //{
+        //    var collection = GetTasksCollectionForEdit();
+        //    try
+        //    {
+        //        collection.InsertOne(task);
+        //    }
+        //    catch (MongoCommandException ex)
+        //    {
+        //        string msg = ex.Message;
+        //    }
+        //}
+
+        //private IMongoCollection<MyTask> GetTasksCollection()
+        //{
+        //    MongoClientSettings settings = new MongoClientSettings();
+        //    settings.Server = new MongoServerAddress(host, 10255);
+        //    settings.UseSsl = true;
+        //    settings.SslSettings = new SslSettings();
+        //    settings.SslSettings.EnabledSslProtocols = SslProtocols.Tls12;
+
+        //    MongoIdentity identity = new MongoInternalIdentity(dbName, userName);
+        //    MongoIdentityEvidence evidence = new PasswordEvidence(password);
+
+        //    settings.Credentials = new List<MongoCredential>()
+        //    {
+        //        new MongoCredential("SCRAM-SHA-1", identity, evidence)
+        //    };
+
+        //    MongoClient client = new MongoClient(settings);
+        //    var database = client.GetDatabase(dbName);
+        //    var todoTaskCollection = database.GetCollection<MyTask>(collectionName);
+        //    return todoTaskCollection;
+        //}
+
+        //private IMongoCollection<MyTask> GetTasksCollectionForEdit()
+        //{
+        //    MongoClientSettings settings = new MongoClientSettings();
+        //    settings.Server = new MongoServerAddress(host, 10255);
+        //    settings.UseSsl = true;
+        //    settings.SslSettings = new SslSettings();
+        //    settings.SslSettings.EnabledSslProtocols = SslProtocols.Tls12;
+
+        //    MongoIdentity identity = new MongoInternalIdentity(dbName, userName);
+        //    MongoIdentityEvidence evidence = new PasswordEvidence(password);
+
+        //    settings.Credentials = new List<MongoCredential>()
+        //    {
+        //        new MongoCredential("SCRAM-SHA-1", identity, evidence)
+        //    };
+        //    MongoClient client = new MongoClient(settings);
+        //    var database = client.GetDatabase(dbName);
+        //    var todoTaskCollection = database.GetCollection<MyTask>(collectionName);
+        //    return todoTaskCollection;
+        //}
+
+        # region IDisposable
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                }
+            }
+
+            this.disposed = true;
+        }
+
+        # endregion
+    }
+}
---
+++ Core.Models/AirbnbListing.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Models
+{
+    public class AirbnbListing
+    {
+        public object ID { get; set; }
+        public string ListingUrl { get; set; }
+        public object ScrapeID { get; set; }
+        public DateTime LastScraped { get; set; }
+        public string Name { get; set; }
+        public string Summary { get; set; }
+        public object Space { get; set; }
+        public string Description { get; set; }
+        public string ExperiencesOffered { get; set; }
+        public object NeighborhoodOverview { get; set; }
+        public object Notes { get; set; }
+        public object Transit { get; set; }
+        public object Access { get; set; }
+        public object Interaction { get; set; }
+        public object HouseRules { get; set; }
+        public string ThumbnailUrl { get; set; }
+        public string MediumUrl { get; set; }
+        public object PictureUrl { get; set; }
+        public string XLPictureUrl { get; set; }
+        public object HostID { get; set; }
+        public string HostURL { get; set; }
+        public string HostName { get; set; }
+        public DateTime HostSince { get; set; }
+        public string HostLocation { get; set; }
+        public string HostAbout { get; set; }
+        public string HostResponseTime { get; set; }
+        public object HostResponseRate { get; set; }
+        public string HostAcceptanceRate { get; set; }
+        public string HostThumbnailUrl { get; set; }
+        public string HostPictureUrl { get; set; }
+        public string HostNeighbourhood { get; set; }
+        public int HostListingsCount { get; set; }
+        public int HostTotalListingsCount { get; set; }
+        public string HostVerifications { get; set; }
+        public string Street { get; set; }
+        public string Neighbourhood { get; set; }
+        public string NeighbourhoodCleansed { get; set; }
+        public object NeighbourhoodGroupCleansed { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public DateTime Zipcode { get; set; }
+        public string Market { get; set; }
+        public string SmartLocation { get; set; }
+        public string CountryCode { get; set; }
+        public string Country { get; set; }
+        public object Latitude { get; set; }
+        public object Longitude { get; set; }
+        public string PropertyType { get; set; }
+        public string RoomType { get; set; }
+        public int Accommodates { get; set; }
+        public float Bathrooms { get; set; }
+        public int Bedrooms { get; set; }
+        public int Beds { get; set; }
+        public string BedType { get; set; }
+        public string Amenities { get; set; }
+        public object SquareFeet { get; set; }
+        public object Price { get; set; }
+        public object WeeklyPrice { get; set; }
+        public object MonthlyPrice { get; set; }
+        public object SecurityDeposit { get; set; }
+        public object CleaningFee { get; set; }
+        public int GuestsIncluded { get; set; }
+        public object ExtraPeople { get; set; }
+        public int MinimumNights { get; set; }
+        public int MaximumNights { get; set; }
+        public string CalendarUpdated { get; set; }
+        public object HasAvailability { get; set; }
+        public int Availability30 { get; set; }
+        public int Availability60 { get; set; }
+        public int Availability90 { get; set; }
+        public int Availability365 { get; set; }
+        public DateTime CalendarlastScraped { get; set; }
+        public int NumberofReviews { get; set; }
+        public DateTime FirstReview { get; set; }
+        public DateTime LastReview { get; set; }
+        public DateTime ReviewScoresRating { get; set; }
+        public DateTime ReviewScoresAccuracy { get; set; }
+        public DateTime ReviewScoresCleanliness { get; set; }
+        public DateTime ReviewScoresCheckin { get; set; }
+        public DateTime ReviewScoresCommunication { get; set; }
+        public DateTime ReviewScoresLocation { get; set; }
+        public DateTime ReviewScoresValue { get; set; }
+        public object License { get; set; }
+        public string JurisdictionNames { get; set; }
+        public string CancellationPolicy { get; set; }
+        public int Calculatedhostlistingscount { get; set; }
+        public string ReviewsperMonth { get; set; }
+        public object Geolocation { get; set; }
+        public object Features { get; set; }
+        public string _id { get; set; }
+    }
+}
